9387ee0b93bb9843bc49e93f87bcbdec5726f73c
Kristen Sumiko Jones <kristensumiko@gmail.com>
2020-08-04 06:07:30 -0400

    revisions made - 2nd submisson

--- index.js
+++ index.js
@@ -1,135 +1,144 @@
-function renderStartPage() {
-    const startPageHtml = $(`<div id="startpage">
+function generateStartPageHtml() {
+  const startPageHtml = $(`<div class="startPage">
     <div><button type="button" class="start">START</button></div>
     <div><p><span class="Blood">ARE YOU A TRUE FAN</span>?</p></div>
     </div>`);
-    $('.startPage').html(startPageHtml);
-    $('.quizPage').hide();
-  }
+  return startPageHtml;
+}
+
+function renderStartPage() {
+  $('main').html(generateStartPageHtml());
+}
   
-  function startQuiz() {
-    $('.startPage').on('click', '.start', function(event) {
-      $('.startPage').hide();
-      $('.quizPage').show();
-      $('body').removeClass('startPgImg').addClass('questionsImg');
-      renderQuestion();
-      generateOptionsHtml();
-      renderQuestionNumberAndScore();
-    });
+function startQuiz() {
+  $('main').on('click', '.start', function(event) {
+    $('body').removeClass('startPgImg').addClass('questionsImg');
+    renderQuizPage();
+  });
+}
+
+function generateQuestion() {
+  let question = STORE.questions[STORE.currentQuestion]
+  let questionHtml = $(
+    `<form class='quiz'>
+    <legend>${question.question}</legend>
+    <div class='jsOptions'>${generateOptionsHtml()}</div>
+    <button class='submit' type='submit'>SUBMIT</button>
+    <button class='continue' type='button'>CONTINUE</button>
+    </form>
+    <div class='questionNumAndScore'>${generateQuestionNumberAndScore()}</div>`);
+  return questionHtml;
+}
+
+function generateOptionsHtml() {
+  let question = STORE.questions[STORE.currentQuestion]
+  let optionsHtml = ''
+  for(let i=0; i<question.options.length; i++) {
+    optionsHtml += `<div><label><input type='radio' name='option' value='${question.options[i]}'>
+    ${question.options[i]}</label></div>`;
   }
-  
-  function renderQuestion() {
-    let question = STORE.questions[STORE.currentQuestion]
-    let questionHtml = $(`<form class='quiz'>
-      <legend>${question.question}</legend>
-      <div class='jsOptions'>${generateOptionsHtml()}</div>
-      <button class='submit' type='submit'>SUBMIT</button>
-      <button class='continue' type='button'>CONTINUE</button>
-      </form>`);
-    $('.quizPage').html(questionHtml);
-    $('.continue').hide();
-  }
-  function generateOptionsHtml() {
-    let question = STORE.questions[STORE.currentQuestion]
-    let optionsHtml = ''
-    for(let i=0; i<question.options.length; i++) {
-      optionsHtml += `<div><input type='radio' name='option' value='${question.options[i]}'>
-      <label for="${question.options[i]}">${question.options[i]}</label></div>`;
+  return optionsHtml;
+}
+
+function generateQuestionNumberAndScore() {
+  return `<p>Question ${STORE.currentQuestion+1}\/10</p><p>Answers Correct: ${STORE.correctAnswerCount}</p>`;
+}
+
+function renderQuizPage() {
+  $('main').html(generateQuestion(), generateOptionsHtml(), generateQuestionNumberAndScore());
+  $('.continue').hide();
+}
+
+function generateRightAnswerFeedback() {
+  return `<span class="right below"> &#10003 You're right!</span>`;
+}
+function generateWrongAnswerFeedback() {
+  return `<span class="wrong below"> &#10007 Not quite.</span>`;
+}
+function generateAnswerCorrectionFeedback() {
+  return `<span class="correction"> &#9666 Duh!</span>`;
+}
+
+function handleFormSubmit() {
+  $('main').on('submit', 'form', function(event) {
+    event.preventDefault();
+    let selectedAnswer = $(`input[name=option]:checked`).val();
+    let correctAnswer = STORE.questions[STORE.currentQuestion].answer;
+    if (!selectedAnswer) {
+      alert ("You didn't select an answer!");
+    return;
     }
-    return optionsHtml;
-  }
-  
-  function renderQuestionNumberAndScore() {
-    $('.questionNumAndScore').html(`<div><p>Question ${STORE.currentQuestion+1}\/10</p><p>Answers Correct: ${STORE.correctAnswerCount}</p></div>`);
-  }
-  
-  function handleFormSubmit() {
-    $('.quizPage').on('submit', 'form', function(event) {
-      event.preventDefault();
-      let selectedAnswer = $('input[name=option]:checked').val();
-      let correctAnswer = STORE.questions[STORE.currentQuestion].answer;
-      if (!selectedAnswer) {
-        alert ("You didn't select an answer!");
-        return;
-        }
-        else if (selectedAnswer === correctAnswer) {
-          $('input[name=option]:checked').siblings('label').append( `<span class="right"> &#10003 You're right!</span>`);
-          //update correctAnswerCount
-          STORE.correctAnswerCount ++;       
-        }
-        else {
-          $('input[name=option]:checked').siblings('label').append(`<span class="wrong"> &#10007 Not quite.</span>`);
-          $(`input[value="${correctAnswer}"]`).siblings('label').append(`<span class="correction"> &#9666 Duh!</span>`);
-        }
-        $('.submit').hide();
-        $('.continue').show();
-        $('input').prop('disabled', true);
-        renderQuestionNumberAndScore();
-    })
-  }
-  
-  function nextQuestion() {
-    $('.quizPage').on('click', '.continue', function(event) {
-      //and add 1 to question number
-      if (STORE.currentQuestion+1 === STORE.questions.length) {
-        finishQuiz();
-      } else {
-        STORE.currentQuestion++;
-        $('.submit').show();
-        $('input').prop('disabled', false);
-        renderQuestion();
-        generateOptionsHtml();
-        renderQuestionNumberAndScore();
-      }
-    })
-  }
-  
-  function finishQuiz() {
-    $('body').removeClass('questionsImg').addClass('resultsImg');
-    $('.questionNumAndScore').hide();
-    $('.quizPage').html(renderFeedbackPage());
-  }
-  
-  function renderFeedbackPage() {
-    return `<div class='score'>${STORE.correctAnswerCount * 10} %</div>
-    <div class='feedback'>${renderAppropriateFeedback()}</div>
-    <button class='restart' type='button'>RESTART</button>`;
-  }
-  
-  function renderAppropriateFeedback() {
-    let feedbackHtml = ''
-    if (STORE.correctAnswerCount === 0) {
-      feedbackHtml += `<p>ZERO?! Clearly NOT a fan.</p>`
-    } else if (STORE.correctAnswerCount <= 4) {
-      feedbackHtml += `<p>Shotty memory, not even 50%.</p>`;
-    } else if (STORE.correctAnswerCount <= 7) {
-      feedbackHtml += `<p>It did air a long time ago, you did ok.</p>`;
-    } else if (STORE.correctAnswerCount <= 9) {
-      feedbackHtml += `<p>Good job, almost got 'em all!</p>`;
+    else if (selectedAnswer === correctAnswer) {
+      $(`input[name=option]:checked`).parent('label').append(generateRightAnswerFeedback());
+      STORE.correctAnswerCount ++;       
+    }
+    else {
+      $(`input[name=option]:checked`).parent('label').append(generateWrongAnswerFeedback());
+      $(`input[value='${correctAnswer}']`).parent('label').append(generateAnswerCorrectionFeedback());
+    }
+    $('.submit').hide();
+    $('.continue').show();
+    $('input').prop('disabled', true);
+    generateQuestionNumberAndScore();
+  })
+}
+
+function nextQuestion() {
+  $('main').on('click', '.continue', function(event) {
+    if (STORE.currentQuestion+1 === STORE.questions.length) {
+      finishQuiz();
     } else {
-      feedbackHtml += `<p>Amazing! You are a TRUE FAN!</p>`;
+      STORE.currentQuestion++;
+      $('.submit').show();
+      $('input').prop('disabled', false);
+      renderQuizPage();
     }
-    return feedbackHtml;
+  })
+}
+
+function generateQuizResults() {
+  let resultsHtml = ''
+  if (STORE.correctAnswerCount === 0) {
+    resultsHtml += `<p>ZERO?! Clearly NOT a fan.</p>`
+  } else if (STORE.correctAnswerCount <= 4) {
+    resultsHtml += `<p>Shotty memory, not even 50%.</p>`;
+  } else if (STORE.correctAnswerCount <= 7) {
+    resultsHtml += `<p>It did air a long time ago, you did ok.</p>`;
+  } else if (STORE.correctAnswerCount <= 9) {
+    resultsHtml += `<p>Good job, almost got 'em all!</p>`;
+  } else {
+    resultsHtml += `<p>Amazing! You are a TRUE FAN!</p>`;
   }
-  
-  function handleRestartButton() {
-    $('.quizPage').on('click', '.restart', function (event) {
-      STORE.currentQuestion = 0;
-      STORE.correctAnswerCount = 0;
-      $('body').removeClass('resultsImg').addClass('questionsImg');
-      $('.questionNumAndScore').show();
-      renderQuestion();
-      generateOptionsHtml();
-      renderQuestionNumberAndScore();
-    })
-  }
-  
-  function runQuizApp() {
-    renderStartPage();
-    startQuiz();
-    handleFormSubmit();
-    nextQuestion();
-    handleRestartButton();
-  }
-  
-  $(runQuizApp);
+  return resultsHtml;
+}
+
+function finishQuiz() {
+  $('body').removeClass('questionsImg').addClass('resultsImg');
+  $('.questionNumAndScore').remove();
+  $('main').html(renderFeedbackPage());
+}
+
+function renderFeedbackPage() {
+  return `<div class='score'>${STORE.correctAnswerCount * 10}%</div>
+  <div class='feedback'>${generateQuizResults()}</div>
+  <button class='restart' type='button'>RESTART</button>`;
+}
+
+function handleRestartButton() {
+  $('main').on('click', '.restart', function (event) {
+    STORE.currentQuestion = 0;
+    STORE.correctAnswerCount = 0;
+    $('body').removeClass('resultsImg').addClass('questionsImg');
+    renderQuizPage();
+  })
+}
+
+function runQuizApp() {
+  renderStartPage();
+  startQuiz();
+  handleFormSubmit();
+  nextQuestion();
+  handleRestartButton();
+}
+
+$(runQuizApp);
